137941ed7ce9da8521a24db7806dd96603d988bd
Rierison <rierisonpinheiro@gmail.com>
2025-12-26 00:52:41 -0300

    git init inciado

---
+++ aula1.py
@@ -0,0 +1,15 @@
+"""
+Docstring  
+Escrever o que eu quiser aqui
+"""
+
+'''
+Usar para escrever sua notas 
+ou explicar o seu código
+'''
+
+
+# Permite escrever um comentário em Python
+print(123) # Exibe ou imprima uma mensagem na tela
+# Abaixo
+print(456)  # Outro comentário
---
+++ aula2.py
@@ -0,0 +1,8 @@
+# \r\n -> CRLF   \r\n - Quebra de linha
+# \n ->  Utilizado mo Sistema Linux - quebra de linha.
+
+# sep='' - Argumento separador nomeado
+# end='' - Final de linha, quebra ou insere um caractere
+
+print(12, 34, sep="-", end='\r\n')   # Argumentos não nomeados que ficam entre os parênteses
+print(56, 78, sep='-')   # Estou passamos vários argumentos e quero a função print exiba no ambiente virtual
