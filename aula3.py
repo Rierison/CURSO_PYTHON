f76c6e65f30ce7df988d3fb75548f699c82e7b04
Rierison <rierisonpinheiro@gmail.com>
2025-12-26 02:02:05 -0300

    DocStrings

--- aula3.py
+++ aula3.py
@@ -1,14 +1,23 @@
 """
+DocStrings
 Python = Linguagem de programação
 Tipo de tiapgem = Dinâmica / Forte
 str -> string -> texto
 Strings são textos que estão dentro de aspas
 """
+print(1234)
 
 # Aspas simples
+print('Rierison Pinheiro')
+print(1, 'Rierison "Pinheiro"') # É permitido ASPAS DUPLAS dentro de ASPAS SIMPLES
+
 
 #Aspas duplas
+print("Rierison Pinheiro")
+print(2, "Rierison 'Pinheiro'")
 
 # Escape
-
+print("Rierison \"Pinheiro\"") # \ Barra invertida - Caractere de Escapa
+ 
 # r
+print(r"Rierison \"Pinheiro\"")
