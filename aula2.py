ad649859d9837ac6a1fcba5eecaaf52adbea1130
Rierison <rierisonpinheiro@gmail.com>
2025-12-26 01:02:15 -0300

    Strings são textos que estão dentro de aspas

--- aula2.py
+++ aula2.py
@@ -4,9 +4,7 @@
 # sep='' - Argumento separador nomeado
 # end='' - Final de linha, quebra ou insere um caractere
 
-print(12, 34, sep="-", end='\r\n')   # Argumentos não nomeados que ficam entre os parênteses
-print(56, 78, sep='-')   # Estou passamos vários argumentos e quero a função print exiba no ambiente virtual
-
-
-
+print(12, 34, sep="-", end='#')   # Argumentos não nomeados que ficam entre os parênteses
+print(56, 78, sep='-', end='\n')   # Estou passamos vários argumentos e quero a função print exiba no ambiente virtual
+print(9, 10, sep='-', end='\n')
 
---
+++ aula3.py
@@ -0,0 +1,14 @@
+"""
+Python = Linguagem de programação
+Tipo de tiapgem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+
+#Aspas duplas
+
+# Escape
+
+# r
